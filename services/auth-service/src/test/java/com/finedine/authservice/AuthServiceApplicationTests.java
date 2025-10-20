af9952c640be6988bd1e8d36b1bff8f026906ab4
heyEdem <akatoaugustine@gmail.com>
2025-10-20 16:47:24 +0000

    Gitguardian fix

--- services/auth-service/src/test/java/com/finedine/authservice/AuthServiceApplicationTests.java
+++ services/auth-service/src/test/java/com/finedine/authservice/AuthServiceApplicationTests.java
@@ -3,18 +3,9 @@ package com.finedine.authservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-        "spring.config.import=",
-        "spring.datasource.url=jdbc:h2:mem:testdb",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "server.servlet.context-path=/vehicle-service"
-
-})
 class AuthServiceApplicationTests {
 
     @Test

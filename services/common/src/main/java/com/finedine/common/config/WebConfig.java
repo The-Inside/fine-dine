b5b11ae2d314e3989627c5b5ecd4284f518f5384
heyEdem <akatoaugustine@gmail.com>
2025-11-07 19:39:43 +0000

    feat: resolve conversations

--- services/common/src/main/java/com/finedine/common/config/WebConfig.java
+++ services/common/src/main/java/com/finedine/common/config/WebConfig.java
@@ -18,5 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(rateLimitInterceptor)
                 .addPathPatterns("/api/**");
-        log.info("RateLimitInterceptor registered!")    }
+        log.info("RateLimitInterceptor registered!");
+    }
 }
